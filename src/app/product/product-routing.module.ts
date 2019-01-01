27ca317e604e5bbce35964dc109bf9b66d5dfd3a
Tiep Phan <tieppt12@gmail.com>
2019-01-01 21:15:09 +0700

    done lesson 3

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductComponent } from './product.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
 const productRoutes: Routes = [
   {
     path: 'product',
-    component: ProductListComponent
-  },
-  {
-    path: 'product/:id',
-    component: ProductDetailComponent
+    component: ProductComponent,
+    children: [
+      {
+        path: '',
+        component: ProductListComponent
+      },
+      {
+        path: ':id',
+        children: [
+          {
+            path: '',
+            component: ProductDetailComponent,
+          },
+          {
+            path: 'edit',
+            component: ProductEditComponent,
+          }
+        ]
+      }
+    ]
   }
 ];
 
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
 
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductRoutingModule } from './product-routing.module';
+import { ProductComponent } from './product.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ProductComponent,
+    ProductEditComponent
   ],
   imports: [
     CommonModule,
